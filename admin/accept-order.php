e12a99403d9af8cc77056e3bd75ff087d6f2e402
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-10-17 04:23:24 +0530

    gsdsfs

--- admin/accept-order.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../db/dbConnection.php';
-$id=$_GET['id'];
-$sql2 = mysqli_query($connection,"UPDATE user_order SET status = 1   WHERE order_id = $id");
-header("location: list-order.php");
-?>
